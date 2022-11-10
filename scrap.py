08ee1d3fe556e2401398e43dd956b4f61d4ba18d
Ankan-Basu <60862627+Ankan-Basu@users.noreply.github.com>
2022-11-10 14:04:13 +0530

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,40 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 
-# url = 'https://www.google.com/search?q=sikkim&rlz=1C1ONGR_enIN1025IN1025&oq=sikkim&aqs=chrome..69i57.1480j0j1&sourceid=chrome&ie=UTF-8'
-url = 'https://www.google.com/search?q=darjeeling&rlz=1C1ONGR_enIN1025IN1025&oq=darjeeling&aqs=chrome..69i57j0i271l2.2536j0j9&sourceid=chrome&ie=UTF-8'
+def scraper(query):
+    # url = 'https://www.google.com/search?q=sikkim&rlz=1C1ONGR_enIN1025IN1025&oq=sikkim&aqs=chrome..69i57.1480j0j1&sourceid=chrome&ie=UTF-8'
+    # url = 'https://www.google.com/search?q=darjeeling&rlz=1C1ONGR_enIN1025IN1025&oq=darjeeling&aqs=chrome..69i57j0i271l2.2536j0j9&sourceid=chrome&ie=UTF-8'
 
-resp = requests.get(url)
+    url = f'https://www.google.com/search?q={query}'
+    resp = requests.get(url)
 
-print(resp)
+    print(resp)
 
-soup = BeautifulSoup(resp.content, 'html.parser');
+    soup = BeautifulSoup(resp.content, 'html.parser');
 
+    x = soup.find_all('div', class_='egMi0 kCrYT')
 
-# with open('resp.html', 'w') as file:
-#     file.write(str(soup))
+    results = []
+    cnt = 0
+    with open('a.html', 'w') as file:
+        for elem in x:
+            cnt += 1
+            y = elem.children
+            z = next(y)
+            zzz = z.attrs['href']
+            zz = z.find('div', class_='BNeawe vvjwJb AP7Wnd').string
+            zzzz = str(zz) + '\n', str(zzz) + '\n', '\n\n'
+            results.append(zzzz)
 
-# print(soup.prettify())
+            if (cnt == 5):
+                break
+        
+        for result in results:
+            for elem in result:
+                file.write(elem)
 
-x = soup.find_all('div', class_='egMi0 kCrYT')
+def query_generator(search_word):
+    tmp = search_word.split()
+    res ='+'.join(tmp)
+    return res
 
-results = []
-cnt = 0
-with open('a.html', 'w') as file:
-    for elem in x:
-        cnt += 1
-        y = elem.children
-        # break
-        z = next(y)
-        zzz = z.attrs['href']
-        zz = z.find('div', class_='BNeawe vvjwJb AP7Wnd').string
-        zzzz = str(zz) + '\n', str(zzz) + '\n', '\n\n'
-        results.append(zzzz)
-
-        if (cnt == 5):
-            break
-    
-    for result in results:
-        for elem in result:
-            file.write(elem)
-# print(x)
+if __name__ == '__main__':
+    search_word = input('Enter search word: ')
+    query = query_generator(search_word)
+    scraper(query)
